15e85fe252145d39e23120f73c17c9e7ac1c7511
brkds <1691987839@qq.com>
2025-04-21 16:14:59 +0800

    update

--- test.rs
+++ test.rs
@@ -1,8 +1,8 @@
 use std::collections::HashMap;
 fn main() {
-    let mut map = HashMap::new();
-	map.insert(')','(');
-	map.insert(']','[');
-	map.insert('}','{');
-    println!("Hello, world!  {:?}",map.values());
+	let some_number: Option<i32> = Some(5);
+	let none_number: Option<i32> = None;
+	
+	println!("{}", some_number.unwrap_or(0));  // 输出 5
+	println!("{}", none_number.unwrap_or(0));   // 输出 0
 }
