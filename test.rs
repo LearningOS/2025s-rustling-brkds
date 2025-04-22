c2d32be4776ed43aa8d3601a0dd9a6989e1d54be
brkds <1691987839@qq.com>
2025-04-22 11:37:12 +0800

    update

--- test.rs
+++ test.rs
@@ -1,8 +1,24 @@
-use std::collections::HashMap;
+#[derive(Debug)]
+enum List {
+    Cons(Rc<RefCell<i32>>, Rc<List>),
+    Nil,
+}
+
+use crate::List::{Cons, Nil};
+use std::cell::RefCell;
+use std::rc::Rc;
+
 fn main() {
-	let some_number: Option<i32> = Some(5);
-	let none_number: Option<i32> = None;
-	
-	println!("{}", some_number.unwrap_or(0));  // 输出 5
-	println!("{}", none_number.unwrap_or(0));   // 输出 0
+    let value = Rc::new(RefCell::new(5));
+
+    let a = Rc::new(Cons(Rc::clone(&value), Rc::new(Nil)));
+
+    let b = Cons(Rc::new(RefCell::new(3)), Rc::clone(&a));
+    let c = Cons(Rc::new(RefCell::new(4)), Rc::clone(&a));
+
+    *value.borrow_mut() += 10;
+
+    println!("a after = {a:?}");
+    println!("b after = {b:?}");
+    println!("c after = {c:?}");
 }
