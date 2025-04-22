2e3fd85138d3dca15d4f3c030bad914f6e90e787
brkds <1691987839@qq.com>
2025-04-22 11:48:57 +0800

    update

--- exercises/tests/build.rs
+++ exercises/tests/build.rs
@@ -3,20 +3,13 @@
 //! You should modify this file to make both exercises pass.
 
 fn main() {
-    // In tests7, we should set up an environment variable
-    // called `TEST_FOO`. Print in the standard output to let
-    // Cargo do it.
+    // For tests7 - 保持不变
     let timestamp = std::time::SystemTime::now()
         .duration_since(std::time::UNIX_EPOCH)
         .unwrap()
-        .as_secs(); // What's the use of this timestamp here?
-        println!("cargo:rustc-env=TEST_FOO={}", timestamp);
+        .as_secs();
+    println!("cargo:rustc-env=TEST_FOO={}", timestamp);
 
-        // For tests8 - enable "pass" feature
-        println!("cargo:rustc-cfg=pass");
-
-    // In tests8, we should enable "pass" feature to make the
-    // testcase return early. Fill in the command to tell
-    // Cargo about that.
-
+    // For tests8 - 修正特性设置方式
+    println!("cargo:rustc-cfg=feature=\"pass\"");
 }
